466686ed4cb09b4bb3144616df4e21ba86ffb1c6
Lulu Sheng <lulu.sheng@shopify.com>
2018-01-03 22:15:29 -0500

    Fixed the CSS Nastiness

--- scripts.js
+++ scripts.js
@@ -2,19 +2,27 @@ $(document).ready(function () {
 
     $(window).resize(function () {
         if ($(window).width() < 1000) {
-            $("#info-box1").css("position-x", "");
-            $("#info-box2").removeAttr("style");
+            $("#info-box1").hide();
             $("#info-box2").css("width", "100%");
             $("#info-box2").css("border-left", "0px");
-            $(".clickable-box").css("width", "100%");
+            $(".zoom-container").css("width", "100%");
         }
 
         if ($(window).width() > 1000) {
-            $(".clickable-box").css("width", "50%");
+            $(".zoom-container").css("width", "50%");
+            $("#info-box1").show();
             $("#info-box2").css("width", "50%");
-            $("#info-box2").css("float", "right");
             $("#info-box2").css("border-left", "2px white solid");
-            
+        }
+    });
+
+    $(window).scroll( function() {
+        var scroll = $(window).scrollTop();
+        if (scroll <= 105)
+        {
+            $("h1").fadeIn(400);
+        } else {
+            $("h1").fadeOut(600);
         }
     });
 });
